d93fe5c275bd36ce1836e5fccc57d1523012e934
vimal Raveendran <vimalraveendra@gmail.com>
2020-03-03 22:33:25 +0100

    created components folder, Title Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-
+import Title from "./components/Title/Title";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Chat App</h1>
+      <Title />
     </div>
   );
 }
