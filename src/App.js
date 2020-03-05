01b33520ee792e5b991fe907c526580eb7d4174e
vimal Raveendran <vimalraveendra@gmail.com>
2020-03-05 22:11:13 +0100

    created config component & upadated package dot json

--- src/App.js
+++ src/App.js
@@ -4,11 +4,35 @@ import MessageList from "./components/MessageList/MessageList";
 import MessageFrom from "./components/MessageFrom/MessageFrom";
 import "./App.css";
 import DUMMY_DATA from "./Data/Dummy-Data";
+import Chatkit from "@pusher/chatkit-client-react";
+import { instanceLocator, testToken, roomId, username } from "./config";
 
 class App extends React.Component {
   state = {
     message: DUMMY_DATA
   };
+
+  componentDidMount() {
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator: instanceLocator,
+      userId: username,
+      tokenProvider: new Chatkit.TokenProvider({
+        url: testToken
+      })
+    });
+    chatManager.connect().then(currentUser => {
+      currentUser.subscribeRoom({
+        roomId: roomId,
+        hooks: {
+          onNewMessage: message => {
+            this.setState({
+              message: [...this.state.message, message]
+            });
+          }
+        }
+      });
+    });
+  }
   render() {
     console.log("message", this.state.message);
     return (
---
+++ src/config.js
@@ -0,0 +1,10 @@
+const instanceLocator = "v1:us1:4a89ff97-44f9-475d-bb49-05049befc783";
+
+const testToken =
+  "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/4a89ff97-44f9-475d-bb49-05049befc783/token";
+
+const username = "perborgen";
+
+const roomId = "9703c1d6-8014-4c01-af1c-c5720b32302c";
+
+export { instanceLocator, testToken, roomId, username };
